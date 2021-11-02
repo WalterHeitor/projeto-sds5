610022752a2c4f1dfe8478ea9bc66a816c8d92f4
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-11-02 18:19:39 -0300

    criaçao dos projetos

---
+++ backend/src/test/java/com/devsuprerior/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuprerior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
