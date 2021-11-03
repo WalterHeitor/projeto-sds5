e5009675b7bcdada00abba0dabe731d0b1577e1a
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-11-03 00:45:30 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from 'components/BarChart';
 import DataTabe from 'components/DataTable';
+import DonutChart from 'components/DonutChart';
 import Footer from 'components/Footer';
 import NavBar from 'components/NavBar';
 import React from 'react';
@@ -6,13 +8,27 @@ import React from 'react';
 function App() {
   return (
     <>
-    <NavBar />
-    <div className="container">
-      <h1 className="text-primary">Olá Mundo Walter Lindão</h1>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Olá Seu Sistema de Vendas SoftWalter</h1>
 
-      <DataTabe />
-    </div>
-    <Footer />
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div>
+          <h5 className="text-primary">Todas as vendas</h5>
+        </div>
+        <DataTabe />
+      </div>
+      <Footer />
     </>
   );
 }
