90a4916be7af9bad72cbf271c4436d3728725db9
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-11-04 02:46:53 -0300

    consertando bug de injeçao com component

--- backend/src/main/java/com/devsuprerior/dsvendas/infrastructura/usecases/SaleService.java
+++ backend/src/main/java/com/devsuprerior/dsvendas/infrastructura/usecases/SaleService.java
@@ -4,21 +4,23 @@ import com.devsuprerior.dsvendas.domain.dto.SaleResponse;
 import com.devsuprerior.dsvendas.domain.entities.Sale;
 import com.devsuprerior.dsvendas.domain.repositories.SaleRepository;
 import com.devsuprerior.dsvendas.infrastructura.database.Postgres.SaleRepositoryData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class SaleService {
 
+    @Autowired
     private SaleRepositoryData saleRepositoryData;
+    @Autowired
     private SaleRepository saleRepository;
 
-    public SaleService(SaleRepositoryData saleRepositoryData, SaleRepository saleRepository) {
-        this.saleRepositoryData = saleRepositoryData;
-        this.saleRepository = saleRepository;
-    }
+
 
     public List<SaleResponse> findAll() {
         List<Sale> sales = saleRepository.findAll(saleRepositoryData);
