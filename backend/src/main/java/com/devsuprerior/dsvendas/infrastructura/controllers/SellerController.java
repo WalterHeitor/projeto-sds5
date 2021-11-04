249a626d9a59c501267f7f00fd5ec46dfc4c54f4
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-11-04 02:50:16 -0300

    criando SellerController.java

---
+++ backend/src/main/java/com/devsuprerior/dsvendas/infrastructura/controllers/SellerController.java
@@ -0,0 +1,7 @@
+package com.devsuprerior.dsvendas.infrastructura.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SellerController {
+}
