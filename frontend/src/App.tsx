d8f024a38780ae808bbb4cc0409495c168864410
walterheitor <walterheitorfo@gmail.com>
2021-11-08 01:51:14 -0300

    feat: paginacao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,8 @@
-import BarChart from 'components/BarChart';
-import DataTabe from 'components/DataTable';
-import DonutChart from 'components/DonutChart';
-import Footer from 'components/Footer';
-import NavBar from 'components/NavBar';
-import React from 'react';
+import Routes from 'Routes';
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Olá Seu Sistema de Vendas SoftWalter</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div>
-          <h5 className="text-primary">Todas as vendas</h5>
-        </div>
-        <DataTabe />
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,36 @@
+import BarChart from 'components/BarChart';
+import DataTabe from 'components/DataTable';
+import DonutChart from 'components/DonutChart';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard Olá Seu Sistema de Vendas SoftWalter</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div>
+          <h5 className="text-primary">Todas as vendas</h5>
+        </div>
+        <DataTabe />
+      </div>
+      <Footer />
+    </>
+    );
+}
+
+export default Dashboard;
